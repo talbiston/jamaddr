e56f0cf984bbfcb8dac1954b724957b320b90a44
talbiston <foxtrot711@gmail.com>
2018-05-31 15:09:45 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='jamaddr',
+    version='0.1',
+    packages=['jamaddr'],
+    url='https://github.com/talbiston/jamaddr.git',
+    license='MIT',
+    author='Jamison Emilio & Todd Albiston',
+    author_email='foxtrot711@gmail.com',
+    description='IP Subnet package'
+)
