4df7844e727fe9c7ba429189d67745b9761adb5f
talbiston <foxtrot711@gmail.com>
2018-05-31 16:37:57 -0500

    update descriptions

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='jamaddr',
     version='0.1',
-    packages=['jamaddr'],
+    packages=find_packages(),
     url='https://github.com/talbiston/jamaddr.git',
     license='MIT',
     author='Jamison Emilio & Todd Albiston',
     author_email='foxtrot711@gmail.com',
-    description='IP Subnet package'
+    description='IP Subnet package',
+    classifiers=(
+        "Programming Language :: Python :: 2 ",
+        "Programming Language :: Python :: 3 ",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
 )
